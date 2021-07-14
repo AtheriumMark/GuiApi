e18798e8f305a7a98b19fb4026288493dfa0fae1
Ather <68028830+AtheriumMark@users.noreply.github.com>
2021-07-14 16:45:56 +0300

    Update themes.lua

--- themes.lua
+++ themes.lua
@@ -1,1 +1,13 @@
-
+return {
+  Dark = {
+		Background0 = Color3.fromRGB(60, 60, 70);
+		Background1 = Color3.fromRGB(41, 41, 51);
+		Background2 = Color3.fromRGB(26, 26, 33);
+		
+		Text0 = Color3.fromRGB(197, 197, 197);
+		Text1 = Color3.fromRGB(139, 139, 139);
+		Text2 = Color3.fromRGB(91, 91, 91);
+		Text3 = Color3.fromRGB(56, 56, 56);
+		Text4 = Color3.fromRGB(0, 0, 0);
+	};
+}
