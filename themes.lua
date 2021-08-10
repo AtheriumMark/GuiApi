8861052d15ccafd9d74100539041f9c7bcaa0ec8
Ather <68028830+AtheriumMark@users.noreply.github.com>
2021-08-10 14:57:02 +0300

    Added Red Theme

--- themes.lua
+++ themes.lua
@@ -3,11 +3,23 @@ return {
 		Background0 = Color3.fromRGB(60, 60, 70);
 		Background1 = Color3.fromRGB(41, 41, 51);
 		Background2 = Color3.fromRGB(26, 26, 33);
-		
+
 		Text0 = Color3.fromRGB(197, 197, 197);
 		Text1 = Color3.fromRGB(139, 139, 139);
 		Text2 = Color3.fromRGB(91, 91, 91);
 		Text3 = Color3.fromRGB(56, 56, 56);
+		Text4 = Color3.fromRGB(0, 0, 0);
+	};
+
+	Red = {
+		Background0 = Color3.fromRGB(60, 0, 10);
+		Background1 = Color3.fromRGB(41, 0, 10);
+		Background2 = Color3.fromRGB(26, 0, 7);
+
+		Text0 = Color3.fromRGB(197, 0, 0);
+		Text1 = Color3.fromRGB(139, 0, 0);
+		Text2 = Color3.fromRGB(91, 0, 0);
+		Text3 = Color3.fromRGB(56, 0, 0);
 		Text4 = Color3.fromRGB(0, 0, 0);
 	};
 }
